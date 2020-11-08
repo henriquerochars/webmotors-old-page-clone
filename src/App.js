0ae1cff55272f98bbd426dd585c8ffb64958b290
Henrique Rocha Serrano <henrique.serrano@meupag.com.br>
2020-11-08 17:05:58 -0300

    add react tabs

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import React from 'react'
 import GlobalStyles from './styles/global'
 
 import Header from './components/Header'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+import 'react-tabs/style/react-tabs.css'
 
 function App() {
   return (
     <>
       <Header />
+
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+
       <GlobalStyles />
     </>
   )
